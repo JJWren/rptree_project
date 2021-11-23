2edeb66f6d72a8ceaa1fe21b4820356562eef330
Joshua Wren <43586816+JJWren@users.noreply.github.com>
2021-11-23 14:28:57 -0600

    Added additional parser args

--- rptree_project/tree.py
+++ rptree_project/tree.py
@@ -7,8 +7,4 @@ from rptree.cli import main
 
 
 if __name__ == "__main__":
-    # region [Header]
-    print('Welcome to RP Tree!')
-    print('Author: Joshua Wren')
-    print(f'Program Start: {datetime.now()}\n')
-    # endregion
+    main()
