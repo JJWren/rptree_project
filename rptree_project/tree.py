065da4290548b242a130b149e4a11c770b33e9f4
Joshua Wren <43586816+JJWren@users.noreply.github.com>
2021-11-23 13:20:02 -0600

    Initial Commit

---
+++ rptree_project/tree.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# tree.py
+
+"""This module provides RP Tree entry point script."""
+
+from rptree.cli import main
+
+
+if __name__ == "__main__":
+    # region [Header]
+    print('Welcome to RP Tree!')
+    print('Author: Joshua Wren')
+    print(f'Program Start: {datetime.now()}\n')
+    # endregion
